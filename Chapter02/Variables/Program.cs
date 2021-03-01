1ea3d4fa0b60d67f6ac0015c3472fb72e03fd608
JerryChuang <jerrychuang@glaibo.com>
2021-03-01 17:30:26 +0800

    Storing dynamic types.

--- Chapter02/Variables/Program.cs
+++ Chapter02/Variables/Program.cs
@@ -12,6 +12,12 @@ namespace Variables
             // int length1 = name.Length; // gives compile error!
             int length2 = ((string)name).Length;
             Console.WriteLine($"{name} has {length2} characters.");
+
+            // storing a string in a dynamic object
+            dynamic anotherName = "Ahmed";
+            // this compiles but would throw an exception at run-time
+            // if you later store a data type that does not have a property named Length
+            int length = anotherName.Length;
         }
     }
 }
