096cbc72b0cba8d2f195adeff0f75169a6123165
JerryChuang <jerrychuang@glaibo.com>
2021-03-01 17:53:59 +0800

    Chapter02 - Specifying and inferring the type of a local variable.

--- Chapter02/Variables/Program.cs
+++ Chapter02/Variables/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Xml;
 
 namespace Variables
 {
@@ -18,6 +20,17 @@ namespace Variables
             // this compiles but would throw an exception at run-time
             // if you later store a data type that does not have a property named Length
             int length = anotherName.Length;
+
+            // good use of var because it avoids the repeated type
+            // as shown in the more verbose second statement
+            var xml1 = new XmlDocument();
+            XmlDocument xml2 = new XmlDocument();
+
+            // bad use of var because we cannot tell the type, so we 
+            // should use a specific type declaration as shown in 
+            // the second statement
+            // var file1 = File.CreateText(@"C:\something.txt");
+            // StreamWriter file2 = File.CreateText(@"C:\something.txt");
         }
     }
 }
