62f13eb8ea88e9707205f91ce27db0b5a85e6e1b
JerryChuang <jerrychuang@glaibo.com>
2021-03-01 20:58:13 +0800

    Declaring non-nullable variables and parameters.

--- Chapter02/NullHandling/Program.cs
+++ Chapter02/NullHandling/Program.cs
@@ -14,6 +14,20 @@ namespace NullHandling
             thisCouldBeNull = 7;
             Console.WriteLine(thisCouldBeNull);
             Console.WriteLine(thisCouldBeNull.GetValueOrDefault());
+
+            var address = new Address();
+            address.Building = null;
+            address.Street = null; // Warning: Cannot convert null literal to non-nullable reference type.
+            address.City = "London";
+            address.Region = null; // Warning: Cannot convert null literal to non-nullable reference type.
         }
+    }
+
+    class Address
+    {
+        public string? Building;
+        public string Street = string.Empty;
+        public string City = string.Empty;
+        public string Region = string.Empty;
     }
 }
