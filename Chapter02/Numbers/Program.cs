79253a9f60cb1731473e3ced6f2f48af65a58196
JerryChuang <jerrychuang@glaibo.com>
2021-03-01 16:54:54 +0800

    Chapter02 - Comparing double and decimal.

--- Chapter02/Numbers/Program.cs
+++ Chapter02/Numbers/Program.cs
@@ -27,6 +27,30 @@ namespace Numbers
             Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}");
             Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range {double.MinValue:N0} to {double.MaxValue:N0}");
             Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range {decimal.MinValue:N0} to {decimal.MaxValue:N0}");
+
+            Console.WriteLine("Using doubles:");
+            double a = 0.1;
+            double b = 0.2;
+            if (a + b == 0.3)
+            {
+                Console.WriteLine($"{a} + {b} equals 0.3");
+            }
+            else
+            {
+                Console.WriteLine($"{a} + {b} does NOT equals 0.3");
+            }
+
+            Console.WriteLine("Using decimals:");
+            decimal c = 0.1m;
+            decimal d = 0.2m;
+            if (c + d == 0.3m)
+            {
+                Console.WriteLine($"{c} + {d} equals 0.3");
+            }
+            else
+            {
+                Console.WriteLine($"{c} + {d} does NOT equals 0.3");
+            }
         }
     }
 }
