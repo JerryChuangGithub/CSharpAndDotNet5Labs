d4618b942241334b9ee7b78189cf0765104e3ec8
JerryChuang <jerrychuang@glaibo.com>
2021-03-01 17:20:33 +0800

    Chapter02 - Storing any type of object.

---
+++ Chapter02/Variables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object height = 1.88;
+            object name = "Amir";
+            Console.WriteLine($"{name} is {height} metres tall.");
+            // int length1 = name.Length; // gives compile error!
+            int length2 = ((string)name).Length;
+            Console.WriteLine($"{name} has {length2} characters.");
+        }
+    }
+}
