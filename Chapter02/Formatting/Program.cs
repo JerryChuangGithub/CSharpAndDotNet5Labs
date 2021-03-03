c8c4e9abda8988f52edede5b03f4343f34448651
JerryChuang <jerrychuang@glaibo.com>
2021-03-03 20:45:10 +0800

    Chapter02 - Understanding format strings.

--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -22,6 +22,24 @@ namespace Formatting
 
             // interpolated strings.
             Console.WriteLine($"{numberOfApples} apples costs {pricePerApple * numberOfApples:C}");
+
+            // Understanding format strings
+            string applesText = "Apples";
+            int applesCount = 1234;
+            string bananasText = "Bananas";
+            int bananasCount = 56789;
+            Console.WriteLine(
+              format: "{0,-8} {1,6:N0}",
+              arg0: "Name",
+              arg1: "Count");
+            Console.WriteLine(
+              format: "{0,-8} {1,6:N0}",
+              arg0: applesText,
+              arg1: applesCount);
+            Console.WriteLine(
+              format: "{0,-8} {1,6:N0}",
+              arg0: bananasText,
+              arg1: bananasCount);
         }
     }
 }
