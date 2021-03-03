87cf2fd90cee8889989e6b28c454acb1221bdc30
JerryChuang <jerrychuang@glaibo.com>
2021-03-03 23:41:48 +0800

    Chapter02 - Simplifying the usage of the console.

--- Chapter02/Formatting/Program.cs
+++ Chapter02/Formatting/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using static System.Console;
 
 namespace Formatting
 {
@@ -9,7 +9,7 @@ namespace Formatting
             // numbered positional arguments
             int numberOfApples = 12;
             decimal pricePerApple = 0.35M;
-            Console.WriteLine(
+            WriteLine(
               format: "{0} apples costs {1:C}",
               arg0: numberOfApples,
               arg1: pricePerApple * numberOfApples);
@@ -21,32 +21,32 @@ namespace Formatting
             //WriteToFile(formatted); // writes the string into a file
 
             // interpolated strings.
-            Console.WriteLine($"{numberOfApples} apples costs {pricePerApple * numberOfApples:C}");
+            WriteLine($"{numberOfApples} apples costs {pricePerApple * numberOfApples:C}");
 
             // Understanding format strings
             string applesText = "Apples";
             int applesCount = 1234;
             string bananasText = "Bananas";
             int bananasCount = 56789;
-            Console.WriteLine(
+            WriteLine(
               format: "{0,-8} {1,6:N0}",
               arg0: "Name",
               arg1: "Count");
-            Console.WriteLine(
+            WriteLine(
               format: "{0,-8} {1,6:N0}",
               arg0: applesText,
               arg1: applesCount);
-            Console.WriteLine(
+            WriteLine(
               format: "{0,-8} {1,6:N0}",
               arg0: bananasText,
               arg1: bananasCount);
 
             // Getting text input from the user
-            Console.Write("Type your first name and press ENTER: ");
-            string firstName = Console.ReadLine();
-            Console.Write("Type your age and press ENTER: ");
-            string age = Console.ReadLine();
-            Console.WriteLine(
+            Write("Type your first name and press ENTER: ");
+            string firstName = ReadLine();
+            Write("Type your age and press ENTER: ");
+            string age = ReadLine();
+            WriteLine(
               $"Hello {firstName}, you look good for {age}.");
         }
     }
