abb33e874806bc931c41fa68489fea8674c71c20
JerryChuang <jerrychuang@glaibo.com>
2021-03-04 20:41:55 +0800

    Chapter02 - Getting text input from the user.

---
+++ Chapter02/Arguments/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace Arguments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+
+            foreach (var arg in args)
+            {
+                WriteLine(arg);
+            }
+        }
+    }
+}
