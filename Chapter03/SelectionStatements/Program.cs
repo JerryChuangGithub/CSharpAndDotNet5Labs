3ff6558b2d6b5f40c569db90c8a23acaef8b2af8
JerryChuang <jerrychuang@glaibo.com>
2021-03-06 23:18:29 +0800

    Chapter03 - Branching with the switch statement.

--- Chapter03/SelectionStatements/Program.cs
+++ Chapter03/SelectionStatements/Program.cs
@@ -1,4 +1,5 @@
-﻿using static System.Console;
+﻿using System;
+using static System.Console;
 
 namespace SelectionStatements
 {
@@ -28,6 +29,31 @@ namespace SelectionStatements
             {
                 WriteLine("o is not an int so it cannot multiply!");
             }
+
+            // Branching with the switch statement
+            A_label:
+            var number = (new Random()).Next(1, 7);
+            WriteLine($"My random number is {number}");
+            switch (number)
+            {
+                case 1:
+                    WriteLine("One");
+                    break; // jumps to end of switch statement 
+                case 2:
+                    WriteLine("Two");
+                    goto case 1;
+                case 3:
+                case 4:
+                    WriteLine("Three or four");
+                    goto case 1;
+                case 5:
+                    // go to sleep for half a second
+                    System.Threading.Thread.Sleep(500);
+                    goto A_label;
+                default:
+                    WriteLine("Default");
+                    break;
+            } // end of switch statement
         }
     }
 }
