5ede41a7c9e1253a27a58ccd0b3ba40230106ac6
JerryChuang <jerrychuang@glaibo.com>
2021-03-07 19:50:36 +0800

    Chapter03 - Looping with the while statement.

---
+++ Chapter03/IterationStatements/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+namespace IterationStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Looping with the while statement
+            int x = 0;
+            while (x < 10)
+            {
+                WriteLine(x);
+                x++;
+            }
+        }
+    }
+}
