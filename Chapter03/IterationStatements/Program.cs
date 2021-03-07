41cfd19facd6ee8a4103dad6f03bb5d16aa0b237
JerryChuang <jerrychuang@glaibo.com>
2021-03-07 22:15:38 +0800

    Chapter03 - Looping with the do statement.

--- Chapter03/IterationStatements/Program.cs
+++ Chapter03/IterationStatements/Program.cs
@@ -13,6 +13,16 @@ namespace IterationStatements
                 WriteLine(x);
                 x++;
             }
+
+            // Looping with the do statement
+            string password = string.Empty;
+            do
+            {
+                Write("Enter your password: ");
+                password = ReadLine();
+            }
+            while (password != "Pa$$w0rd");
+            WriteLine("Correct!");
         }
     }
 }
