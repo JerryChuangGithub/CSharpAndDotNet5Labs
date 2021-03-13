b9aac0d1d238fc637a293ae528443cec4d106f0a
JerryChuang <jerrychuang@glaibo.com>
2021-03-13 16:11:06 +0800

    Chpater03 - Parsing from strings to numbers or dates and times.

--- Chapter03/CastingConverting/Program.cs
+++ Chapter03/CastingConverting/Program.cs
@@ -75,6 +75,25 @@ namespace CastingConverting
             // convert to Base64 string and output as text
             string encoded = Convert.ToBase64String(binaryObject);
             WriteLine($"Binary Object as Base64: {encoded}");
+
+            // Parsing from strings to numbers or dates and times
+            int age = int.Parse("27");
+            DateTime birthday = DateTime.Parse("4 July 1980");
+            WriteLine($"I was born {age} years ago.");
+            WriteLine($"My birthday is {birthday}.");
+            WriteLine($"My birthday is {birthday:D}.");
+
+            Write("How many eggs are there? ");
+            int count;
+            string input = ReadLine();
+            if (int.TryParse(input, out count))
+            {
+                WriteLine($"There are {count} eggs.");
+            }
+            else
+            {
+                WriteLine("I could not parse the input.");
+            }
         }
     }
 }
