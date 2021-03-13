5c47469d36f2206f7f593cdd098514ebb890a859
JerryChuang <jerrychuang@glaibo.com>
2021-03-13 19:41:18 +0800

    Chapter03 - Checking for overflow.

---
+++ Chapter03/CheckingForOverflow/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+
+namespace CheckingForOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Checking for overflow
+            try
+            {
+                checked
+                {
+                    int x = int.MaxValue - 1;
+                    WriteLine($"Initial value: {x}");
+                    x++;
+                    WriteLine($"After incrementing: {x}");
+                    x++;
+                    WriteLine($"After incrementing: {x}");
+                    x++;
+                    WriteLine($"After incrementing: {x}");
+                }
+            }
+            catch (OverflowException)
+            {
+                WriteLine("The code overflowed but I Caught the exception.");
+            }
+
+            unchecked
+            {
+                int y = int.MaxValue + 1;
+                WriteLine($"Initial value: {y}");
+                y--;
+                WriteLine($"After decrementing: {y}");
+                y--;
+                WriteLine($"After decrementing: {y}");
+            }
+        }
+    }
+}
