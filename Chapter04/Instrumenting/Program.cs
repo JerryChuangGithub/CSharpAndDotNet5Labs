f603695b155b2e8fb7b2174324be76d56386ffc3
JerryChuang <jerrychuang@glaibo.com>
2021-03-15 20:37:55 +0800

    Chapter04 - Writing to the default trace listener.

---
+++ Chapter04/Instrumenting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace Instrumenting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Writing to the default trace listener
+            Debug.WriteLine("Debug says, I am watching");
+            Trace.WriteLine("Trace says, I am watching");
+        }
+    }
+}
