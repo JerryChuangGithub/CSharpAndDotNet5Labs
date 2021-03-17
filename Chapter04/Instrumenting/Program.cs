479b279148e749a1906b3c3a6ca776fa60e0ddc7
JerryChuang <jerrychuang@glaibo.com>
2021-03-17 20:56:04 +0800

    Chapter04 - Switching trace levels.

--- Chapter04/Instrumenting/Program.cs
+++ Chapter04/Instrumenting/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
 using System.IO;
 
 namespace Instrumenting
@@ -15,6 +16,21 @@ namespace Instrumenting
             // Writing to the default trace listener
             Debug.WriteLine("Debug says, I am watching");
             Trace.WriteLine("Trace says, I am watching");
+
+            // Switching trace levels
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var ts = new TraceSwitch(displayName: "PacktSwitch", description: "This switch is set via a JSON config.");
+            configuration.GetSection("PacktSwitch").Bind(ts);
+
+            Trace.WriteLineIf(ts.TraceError, "Trace error");
+            Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+            Trace.WriteLineIf(ts.TraceInfo, "Trace informetion");
+            Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
         }
     }
 }
