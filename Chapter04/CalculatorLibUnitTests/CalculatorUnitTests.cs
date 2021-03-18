6e89fa2bf6aa46cb5d207a09f9a78dbb9b5c7cb4
JerryChuang <jerrychuang@glaibo.com>
2021-03-18 23:41:59 +0800

    Chapter04 - Writing unit tests.

--- Chapter04/CalculatorLibUnitTests/CalculatorUnitTests.cs
+++ Chapter04/CalculatorLibUnitTests/CalculatorUnitTests.cs
@@ -1,4 +1,4 @@
-using System;
+using CalculatorLib;
 using Xunit;
 
 namespace CalculatorLibUnitTests
@@ -6,9 +6,30 @@ namespace CalculatorLibUnitTests
     public class CalculatorUnitTests
     {
         [Fact]
-        public void Test1()
+        public void TestAdding2And2()
         {
-
+            // arrange 
+            double a = 2;
+            double b = 2;
+            double expected = 4;
+            var calc = new Calculator();
+            // act
+            double actual = calc.Add(a, b);
+            // assert 
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestAdding2And3()
+        {
+            // arrange 
+            double a = 2;
+            double b = 3;
+            double expected = 6;
+            var calc = new Calculator();
+            // act
+            double actual = calc.Add(a, b);
+            // assert 
+            Assert.Equal(expected, actual);
         }
     }
 }
