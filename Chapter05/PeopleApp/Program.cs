7782c31fafe44724f1592e3ddb994e04bd37f838
JerryChuang <jerrychuang@glaibo.com>
2021-03-31 11:36:30 +0800

    Chapter05 - Storing a value using an enum type.

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -11,10 +11,19 @@ namespace PeopleApp
             var bob = new Person();
             bob.Name = "Bob Smith";
             bob.DateOfBirth = new DateTime(1965, 12, 22);
+            bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
             WriteLine(
                 format: "{0} was born on {1:dddd, d MMMM yyyy}",
                 arg0: bob.Name,
                 arg1: bob.DateOfBirth
+                );
+
+            bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+            WriteLine(
+                format: "{0}'s favorite wonder is {1}. Its integer is {2}",
+                arg0: bob.Name,
+                arg1: bob.FavoriteAncientWonder,
+                arg2: (int)bob.FavoriteAncientWonder
                 );
 
             var alice = new Person
