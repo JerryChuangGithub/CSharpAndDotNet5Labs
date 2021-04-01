09350fe6d7e9ab0d2d6a2f6c479a6fedb5183891
JerryChuang <jerrychuang@glaibo.com>
2021-04-01 08:53:55 +0800

    Chapter05 - Storing multiple values using an enum type.

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -11,19 +11,20 @@ namespace PeopleApp
             var bob = new Person();
             bob.Name = "Bob Smith";
             bob.DateOfBirth = new DateTime(1965, 12, 22);
-            bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
             WriteLine(
                 format: "{0} was born on {1:dddd, d MMMM yyyy}",
                 arg0: bob.Name,
                 arg1: bob.DateOfBirth
                 );
 
-            bob.FavoriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+            // bob.FavoriteAncientWonder = WondersOfTheAncientWorld.HangingGardensOfBabylon |
+            //                             WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
+            bob.FavoriteAncientWonder = (WondersOfTheAncientWorld)11;
             WriteLine(
                 format: "{0}'s favorite wonder is {1}. Its integer is {2}",
                 arg0: bob.Name,
                 arg1: bob.FavoriteAncientWonder,
-                arg2: (int)bob.FavoriteAncientWonder
+                arg2: bob.FavoriteAncientWonder.ToString()
                 );
 
             var alice = new Person
