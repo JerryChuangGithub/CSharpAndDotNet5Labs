5f23dc793710fb2bfeff0a530a0470e83d9cf59f
JerryChuang <jerrychuang@glaibo.com>
2021-04-04 19:57:31 +0800

    Chapter05 - Simplifying data members.

--- Chapter05/PacktLibrary9/Records.cs
+++ Chapter05/PacktLibrary9/Records.cs
@@ -2,6 +2,9 @@
 {
     public class ImmutablePerson
     {
+        // private field by default
+        // int Age;
+    
         public string FirstName { get; init; }
 
         public string LastName { get; init; }
@@ -9,6 +12,8 @@
 
     public record ImmutableVehicle
     {
+        //int Age; // public property equivalent to: public int Age { get; init; }
+
         public int Wheels { get; init; }
 
         public string Color { get; init; }
