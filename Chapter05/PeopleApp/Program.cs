dc194085b79ea1e3971f981c47437e9eb17dbc38
JerryChuang <jerrychuang@glaibo.com>
2021-04-04 17:52:27 +0800

    Chapter05 - Defining flight passengers.

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -152,6 +152,30 @@ namespace PeopleApp
                 format: "{0} earned {1:C} interest.",
                 arg0: gerrierAccount.AccountName,
                 arg1: gerrierAccount.Balance * BankAccount.InterestRate);
+
+            // Defining flight passengers
+            object[] passengers = {
+                new FirstClassPassenger { AirMiles = 1_419 },
+                new FirstClassPassenger { AirMiles = 16_562 },
+                new BusinessClassPassenger(),
+                new CoachClassPassenger { CarryOnKG = 25.7 },
+                new CoachClassPassenger { CarryOnKG = 0 },
+            };
+
+            foreach (object passenger in passengers)
+            {
+                decimal flightCost = passenger switch
+                {
+                    FirstClassPassenger p when p.AirMiles > 35000 => 1500M,
+                    FirstClassPassenger p when p.AirMiles > 15000 => 1750M,
+                    FirstClassPassenger _ => 2000M,
+                    BusinessClassPassenger _ => 1000M,
+                    CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
+                    CoachClassPassenger _ => 650M,
+                    _ => 800M
+                };
+                WriteLine($"Flight costs {flightCost:C} for {passenger}");
+            }
         }
     }
 }
