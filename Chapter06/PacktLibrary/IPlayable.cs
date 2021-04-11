cc37fe033b5dafad61b0276c11866ec2143e2813
JerryChuang <jerrychuang@glaibo.com>
2021-04-11 09:04:59 +0800

    Chapter06 - Defining interfaces with default implementations.

---
+++ Chapter06/PacktLibrary/IPlayable.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+namespace Packt.Shared
+{
+    public interface IPlayable
+    {
+        void Play();
+
+        void Pause();
+
+        // default interface implementation
+        void Stop()
+        {
+            WriteLine("Default implementation of Stop.");
+        }
+    }
+}
